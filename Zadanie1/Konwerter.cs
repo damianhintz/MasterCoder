8eacc7c3147c5ebc7334d04122bc3bb0fa4b005e
Damian Hintz <damian.hintz@gmail.com>
2016-10-04 09:17:36 +0200

    Add task 1 project

---
+++ Zadanie1/Konwerter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLConverter
+{
+    public class Konwerter : ISQLConverter
+    {
+        public override List<SqlQuery> ConvertToSqlInsert(string tabName, string typesBuff, string colNamBuff, string dataBuff)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
