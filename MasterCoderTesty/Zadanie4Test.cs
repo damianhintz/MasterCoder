63501020313de7a0d4dce35427f87c04fb4eb5f5
Damian Hintz <damian.hintz@gmail.com>
2016-10-10 08:39:28 +0200

    Updated: Add more task 4 tests

--- MasterCoderTesty/Zadanie4Test.cs
+++ MasterCoderTesty/Zadanie4Test.cs
@@ -46,5 +46,34 @@ namespace MasterCoderTesty
             var z = f.CalculateCount(number: 30, b: 16);
             Assert.AreEqual(expected: 6, actual: z);
         }
+
+        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void test_czy_zadanie4_tylko_base_10()
+        {
+            var f = Base10SilniaZero.GetInstance();
+            f.CalculateCount(number: 30, b: 11);
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void test_czy_zadanie4_base_10()
+        {
+            var f = Base10SilniaZero.GetInstance();
+            var z = f.CalculateCount(number: 0, b: 10);
+            Assert.AreEqual(expected: 0, actual: z);
+        }
+
+        [TestMethod]
+        public void test_czy_zadanie4_vs_base_10()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                var f10 = Base10SilniaZero.GetInstance();
+                var z10 = f10.CalculateCount(number: i, b: 10);
+                var f = SilniaZero.GetInstance();
+                var z = f.CalculateCount(number: i, b: 10);
+                Assert.AreEqual(expected: z10, actual: z);
+            }
+        }
     }
 }
---
+++ Zadanie4/Base10SilniaZero.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FactorialTrailingZeros
+{
+    public class Base10SilniaZero : IFactorialTrailingZeros
+    {
+        public new static IFactorialTrailingZeros GetInstance()
+        {
+            factorialInstance = new Base10SilniaZero();
+            return factorialInstance;
+        }
+
+        public override int CalculateCount(int number, int b)
+        {
+            if (b != 10) throw new ArgumentOutOfRangeException();
+            return CalculateCount10(number, b);
+        }
+        
+        /// <summary>
+        /// Przypadek dla podstawy 10.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        int CalculateCount10(int number, int b)
+        {
+            //Z = floor(n / 5) + floor(n / 5 ^ 2) + ... + floor(n / 5 ^ k)
+            //k = floor(log5(n))
+            double n = number;
+            int kMax = (int)Math.Floor(Math.Log(n, 5));
+            int fivePower = 5;
+            int z = 0;
+            for (int k = 1; k <= kMax; k++)
+            {
+                z += (int)Math.Floor(n / fivePower);
+                fivePower *= 5;
+            }
+            return z;
+        }
+
+    }
+}
