4f2130c1e271acc76d560940d6cfdca04a1f569c
Damian Hintz <damian.hintz@gmail.com>
2016-10-16 18:47:41 +0200

    Updated: add task 9 tests

--- MasterCoderTesty/Zadanie9Test.cs
+++ MasterCoderTesty/Zadanie9Test.cs
@@ -11,21 +11,49 @@ namespace MasterCoderTesty
     public class Zadanie9Test
     {
         [TestMethod]
+        public void test_czy_zadanie9_przykład_paczki()
+        {
+            var z9 = CardParser.GetInstance();
+            var listener = new ParserListener();
+            z9.setParserListener(listener);
+            var buffer =
+                "BEGIN:VCARD\r\n" +
+                "N:Jan Kowalski\r\n" +
+                "PHOTO:oijasdcokmasodijcocmoijasdcokmasodIjcocmoijasdcokmasodijcocmoijasdcokmasodijco\r\n" +
+                "asodijcocmoijasdcokmasodijcocmoijasdcokmasOdijcocmoijasdcokmasodijcocmoijasdco\r\n" +
+                "asodijcocmoijasdcokYYJrr\r\n" +
+                "\r\n" +
+                "TEL:696 969 696\r\n" +
+                "ADR:ul.Dowborczykow 25, 90 - 993 Lodz\r\n" +
+                "EMAIL:jan.kowalski @cybercom.com\r\n" +
+                "END:VCAR";
+            var bufferChars = buffer.ToCharArray();
+            foreach (var c in bufferChars)
+            {
+                var status = z9.parse(c.ToString());
+                Assert.AreEqual(
+                    expected: ParseStatus.PARSE_STATUS_MORE_DATA,
+                    actual: status, message: "char: " + c);
+                Assert.AreEqual(expected: 0, actual: listener.Count());
+            }
+        }
+
+        [TestMethod]
         public void test_czy_zadanie9_przykład()
         {
             var z9 = CardParser.GetInstance();
             var listener = new ParserListener();
             z9.setParserListener(listener);
             var status = z9.parse(buffer:
-                "BEGIN:VCARD\n" +
-                "N:Jan Kowalski\n" +
-                "PHOTO: oijasdcokmasodijcocmoijasdcokmasodIjcocmoijasdcokmasodijcocmoijasdcokmasodijco\n" +
-                "asodijcocmoijasdcokmasodijcocmoijasdcokmasOdijcocmoijasdcokmasodijcocmoijasdco\n" +
-                "asodijcocmoijasdcokYYJrr\n" +
-                "\n" +
-                "TEL:696 969 696\n" +
-                "ADR:ul.Dowborczykow 25, 90 - 993 Lodz\n" +
-                "EMAIL:jan.kowalski @cybercom.com\n" +
+                "BEGIN:VCARD\r\n" +
+                "N:Jan Kowalski\r\n" +
+                "PHOTO:oijasdcokmasodijcocmoijasdcokmasodIjcocmoijasdcokmasodijcocmoijasdcokmasodijco\r\n" +
+                "asodijcocmoijasdcokmasodijcocmoijasdcokmasOdijcocmoijasdcokmasodijcocmoijasdco\r\n" +
+                "asodijcocmoijasdcokYYJrr\r\n" +
+                "\r\n" +
+                "TEL:696 969 696\r\n" +
+                "ADR:ul.Dowborczykow 25, 90 - 993 Lodz\r\n" +
+                "EMAIL:jan.kowalski @cybercom.com\r\n" +
                 "END:VCARD"
                 );
             Assert.AreEqual(
@@ -54,17 +82,46 @@ namespace MasterCoderTesty
         public void test_czy_zadanie9_more_data()
         {
             var z9 = CardParser.GetInstance();
-            var status = z9.parse(buffer: null);
+            var listener = new ParserListener();
+            z9.setParserListener(listener);
+            var status = z9.parse(buffer:
+                "BEGIN:VCARD\r\n" +
+                "N:Jan Kowalski\r\n" +
+                "PHOTO:\r\n" +
+                "\r\n" +
+                "TEL:696 969 696\r\n" +
+                "ADR:ul.Dowborczykow 25, 90 - 993 Lodz\r\n" +
+                "EMAIL:jan.kowalski @cybercom.com\r\n" +
+                "END:VCARDBEGIN:VCARD"
+                );
             Assert.AreEqual(
                 expected: ParseStatus.PARSE_STATUS_MORE_DATA,
                 actual: status);
+            Assert.AreEqual(expected: 1, actual: listener.Count());
+            var card = listener.First();
+            Assert.AreEqual("Jan Kowalski", card.Name);
+            Assert.AreEqual("", card.PhotoData);
+            Assert.AreEqual("696 969 696", card.TelNumber);
+            Assert.AreEqual("ul.Dowborczykow 25, 90 - 993 Lodz", card.Address);
+            Assert.AreEqual("jan.kowalski @cybercom.com", card.Email);
         }
 
         [TestMethod]
         public void test_czy_zadanie9_error()
         {
             var z9 = CardParser.GetInstance();
-            var status = z9.parse(buffer: null);
+            var listener = new ParserListener();
+            z9.setParserListener(listener);
+            var status = z9.parse(buffer:
+                "BEGIN:VCARD\r\n" +
+                "N:Jan Kowalski\r\n" +
+                "PHOTO:\r\n" +
+                "\r\n" +
+                "TEL:696 969 696\r\n" +
+                "ADR:ul.Dowborczykow 25, 90 - 993 Lodz\r\n" +
+                "EMAIL:jan.kowalski @cybercom.com\r\n" +
+                "END:VCARDX"
+                );
             Assert.AreEqual(
                 expected: ParseStatus.PARSE_STATUS_ERROR,
                 actual: status);
@@ -76,10 +133,13 @@ namespace MasterCoderTesty
             var fileName = Path.Combine(@"..\..\Samples", "vCard0.txt");
             var buffer = File.ReadAllText(fileName, Encoding.GetEncoding(1250));
             var z9 = CardParser.GetInstance();
+            var listener = new ParserListener();
+            z9.setParserListener(listener);
             var status = z9.parse(buffer);
             Assert.AreEqual(
-                expected: ParseStatus.PARSE_STATUS_ERROR,
+                expected: ParseStatus.PARSE_STATUS_MORE_DATA,
                 actual: status);
+            Assert.AreEqual(expected: 0, actual: listener.Count());
         }
 
         [TestMethod]
@@ -88,11 +148,50 @@ namespace MasterCoderTesty
             var fileName = Path.Combine(@"..\..\Samples", "vCard1.txt");
             var buffer = File.ReadAllText(fileName, Encoding.GetEncoding(1250));
             var z9 = CardParser.GetInstance();
-            z9.setParserListener(listener: null);
+            var listener = new ParserListener();
+            z9.setParserListener(listener);
             var status = z9.parse(buffer);
             Assert.AreEqual(
-                expected: ParseStatus.PARSE_STATUS_ERROR,
+                expected: ParseStatus.PARSE_STATUS_OK,
                 actual: status);
+            Assert.AreEqual(expected: 1, actual: listener.Count());
+        }
+
+        [TestMethod]
+        public void test_czy_zadanie9_vCard2()
+        {
+            var fileName = Path.Combine(@"..\..\Samples", "vCard2.txt");
+            var buffer = File.ReadAllText(fileName, Encoding.GetEncoding(1250));
+            var z9 = CardParser.GetInstance();
+            var listener = new ParserListener();
+            z9.setParserListener(listener);
+            var status = z9.parse(buffer);
+            Assert.AreEqual(
+                expected: ParseStatus.PARSE_STATUS_OK,
+                actual: status);
+            Assert.AreEqual(expected: 2, actual: listener.Count());
+        }
+
+        [TestMethod]
+        public void test_czy_zadanie9_vCard2ab()
+        {
+            var fileName = Path.Combine(@"..\..\Samples", "vCard2a.txt");
+            var buffer = File.ReadAllText(fileName, Encoding.GetEncoding(1250));
+            var z9 = CardParser.GetInstance();
+            var listener = new ParserListener();
+            z9.setParserListener(listener);
+            var status = z9.parse(buffer);
+            Assert.AreEqual(
+                expected: ParseStatus.PARSE_STATUS_MORE_DATA,
+                actual: status);
+            Assert.AreEqual(expected: 0, actual: listener.Count());
+            fileName = Path.Combine(@"..\..\Samples", "vCard2b.txt");
+            buffer = File.ReadAllText(fileName, Encoding.GetEncoding(1250));
+            status = z9.parse(buffer);
+            Assert.AreEqual(
+                expected: ParseStatus.PARSE_STATUS_OK,
+                actual: status);
+            Assert.AreEqual(expected: 2, actual: listener.Count());
         }
     }
 }
--- Zadanie9/CardParser.cs
+++ Zadanie9/CardParser.cs
@@ -9,7 +9,8 @@ namespace Parser
     public class CardParser : IParser
     {
         IParserListener _listener;
-        List<string> _lines = new List<string>();
+        StringBuilder _buffer = new StringBuilder();
+        int _lastUnparsedCardLine;
 
         public new static IParser GetInstance()
         {
@@ -19,22 +20,72 @@ namespace Parser
 
         public override ParseStatus parse(string buffer)
         {
-            var card = new vCardStruct();
-            _listener.newVCard(card);
-            var lastLine = _lines.Last();
-            buffer = lastLine + buffer;
+            //if (string.IsNullOrEmpty(buffer)) return ParseStatus.PARSE_STATUS_OK;
+            _buffer.Append(buffer);
+            buffer = _buffer.ToString();
             var lines = buffer.Split(
-                separator: new string[] { @"\n" },
+                separator: new string[] { "\r\n" },
                 options: StringSplitOptions.None);
-            for(int i = 0; i < _lines.Count; i++)
+            vCardStruct card = null;
+            StringBuilder photoData = null;
+            for (int i = _lastUnparsedCardLine; i < lines.Length; i++)
             {
-                var line = _lines[i];
-                if (line.StartsWith(""))
+                var line = lines[i];
+                if (line.Equals("BEGIN:VCARD"))
                 {
-
+                    card = new vCardStruct();
+                    _lastUnparsedCardLine = i;
+                }
+                else if (line.StartsWith("N:"))
+                {
+                    card.Name = line.Substring(2);
+                }
+                else if (line.StartsWith("PHOTO:"))
+                {
+                    photoData = new StringBuilder();
+                    photoData.Append(line.Substring(6));
+                }
+                else if (line.Equals(""))
+                {
+                    if (photoData != null) card.PhotoData = photoData.ToString();
+                    photoData = null;
+                }
+                else if (line.StartsWith("TEL:"))
+                {
+                    card.TelNumber = line.Substring(4);
+                }
+                else if (line.StartsWith("ADR:"))
+                {
+                    card.Address = line.Substring(4);
+                }
+                else if (line.StartsWith("EMAIL:"))
+                {
+                    card.Email = line.Substring(6);
+                }
+                else if (line.Equals("END:VCARD"))
+                {
+                    _listener.newVCard(card);
+                    _lastUnparsedCardLine = i + 1;
+                    card = null;
+                    photoData = null;
+                }
+                else if (line.Equals("END:VCARDBEGIN:VCARD"))
+                {
+                    if (card != null) _listener.newVCard(card);
+                    card = new vCardStruct();
+                    _lastUnparsedCardLine = i;
+                } else
+                {
+                    if (photoData != null) photoData.Append(line);
+                    else
+                    {
+                        if (card != null) return ParseStatus.PARSE_STATUS_ERROR;
+                    }
                 }
             }
-            return ParseStatus.PARSE_STATUS_OK;
+            return card == null ?
+                ParseStatus.PARSE_STATUS_OK :
+                ParseStatus.PARSE_STATUS_MORE_DATA;
         }
 
         public override void setParserListener(IParserListener listener)
